e2cac16a1a44ac61082a2071df756f0214ea9096
Deepak28102000 <deepakrajsandeep@gmail.com>
2023-06-11 20:20:17 +0530

    test

--- src/test/java/com/example/admin/AdminApplicationTests.java
+++ src/test/java/com/example/admin/AdminApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.admin;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AdminApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.example.admin;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class AdminApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
